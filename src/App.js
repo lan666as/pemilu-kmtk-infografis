41693d9ccb9bc1945674f2e12429a752d4ea8785
lan666as <4zidan@gmail.com>
2021-09-05 14:59:00 +0700

    add google analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import GlobalStyle from './globalStyle';
 import ComingSoon from './pages/ComingSoon';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = "G-22R08SG34J"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 library.add(fab)
 
